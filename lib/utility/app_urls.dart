c76c7e262805f190f4a298556d4228a0e5d4599c
CodeByPrime <mrcody497@gmail.com>
2025-11-08 12:22:07 +0500

    feat:add api model and services and api endpoints

---
+++ lib/utility/app_urls.dart
@@ -0,0 +1,8 @@
+import "package:flutter/material.dart";
+
+
+class AppUrls {
+  static const String baseUrl = "https://disease.sh/v3/covid-19/";
+  static const String worldStatesApi = baseUrl + "all";
+  static const String countriesList = baseUrl + "countries";
+}
